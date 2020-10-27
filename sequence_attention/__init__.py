e3709b46c4adcf6efad2c7e1a6d5077edbf86fd9
Zhengqiao <zzhao@staph.dhcp.drexel.edu>
2020-10-27 00:42:52 -0400

    added a new data generator method

--- sequence_attention/__init__.py
+++ sequence_attention/__init__.py
@@ -1,4 +1,4 @@
 from .SeqAttModel import SeqAttModel
 from .SeqVisualUnit import SeqVisualUnit
-from .utils import preprocess_data
-from .DataGenerator import DataGenerator, DataGeneratorUnlabeled
+from .utils import preprocess_data, preprocess_data_pickle
+from .DataGenerator import DataGenerator, DataGeneratorUnlabeled, DataGeneratorPickle, DataGeneratorUnlabeledPickle
